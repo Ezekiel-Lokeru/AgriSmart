bccc2b31ae05a7e74e9494493209cd6bec9b667c
Unknownymous-404-sudo <ezekiellokeru@gmail.com>
2025-10-08 11:54:38 +0300

    controllers updated

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,5 +4,6 @@ const crops = require('./crops');
 const weather = require('./weather');
 const profile = require('./profile');
 const errors = require('./error');
+const admin = require('./admin');
 
-module.exports = { auth , queries , crops , weather , profile , errors };
+module.exports = { auth , queries , crops , admin, weather , profile , errors };
