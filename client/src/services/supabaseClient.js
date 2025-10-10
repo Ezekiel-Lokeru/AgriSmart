1bb02d177e2694bb005269e49dd3b1301cb508ec
Unknownymous-404-sudo <ezekiellokeru@gmail.com>
2025-10-10 23:23:21 +0300

    Configured frontend to use Render backend and Supabase env vars

--- client/src/services/supabaseClient.js
+++ client/src/services/supabaseClient.js
@@ -4,5 +4,6 @@ import { createClient } from "@supabase/supabase-js";
 // Replace with your Supabase project details
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export default supabase;
